32d495fec4b2002d45baf2ef641136a9962ad7e7
RandomStranger0 <162653173+RandomStranger0@users.noreply.github.com>
2024-03-12 23:28:14 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 export const projects = [
     {
       index: 1,
-      name: "EcoConnect",
-      description: "A platform connecting individuals with eco-friendly businesses and products, promoting sustainability.",
+      name: "Dialogue international",
+      description: "Un échange virtuel avec les étudiants à l'international et principalement de l'université de Dallas. Le but était de promouvoir les objectifs de développement durables des nations unies parmi les jeunes. Entre autre l'égalité des genres, lutte contre le changement climatique et intégration sociale.",
       tags: [
         { name: "React", color: "bg-blue-200" }, // Tailwind class for React blue
         { name: "Node.js", color: "bg-green-200" }, // Tailwind class for Node.js green
@@ -13,8 +13,8 @@ export const projects = [
     },
     {
       index: 2,
-      name: "TechTrend",
-      description: "A tech news aggregator that pulls in the latest tech news from various sources, using AI to categorize and rank them.",
+      name: "Concours d'éloquence ESIEE Paris",
+      description: "Un concours organisé par ESIEE Paris pour valoriser l'éloquence. J'ai présenté le thème de la santé mentale et remporté le trophée du Ier espoir d'Eloquence ",
       tags: [
         { name: "Vue.js", color: "bg-green-200" }, // Tailwind class for Vue.js green
         { name: "Express", color: "bg-gray-200" }, // Tailwind class for Express dark
@@ -25,8 +25,8 @@ export const projects = [
     },
     {
       index: 3,
-      name: "FitTrack",
-      description: "A personal fitness tracker that allows users to log workouts, track progress, and set fitness goals.",
+      name: "Zuul-Bad",
+      description: "Zuul-Bad est un jeu vidéo basique d'aventure programmé entièrement en java en première année dans le cadre de mon cursus académique. ",
       tags: [
         { name: "Angular", color: "bg-red-200" }, // Tailwind class for Angular red
         { name: "Firebase", color: "bg-yellow-200" }, // Tailwind class for Firebase yellow
@@ -37,8 +37,8 @@ export const projects = [
     },
     {
       index: 4,
-      name: "CodeCollab",
-      description: "An online code collaboration tool for developers to work together in real-time, featuring live code editing.",
+      name: "Tutorat",
+      description: "Tuteur.",
       tags: [
         { name: "React", color: "bg-blue-200" }, // Tailwind class for React blue
         { name: "Socket.io", color: "bg-gray-200" }, // Tailwind class for Socket.io dark
@@ -46,29 +46,6 @@ export const projects = [
         { name: "Real-time", color: "bg-teal-200" } // Tailwind class for real-time teal
       ],
       source_code_link: "https://github.com/yourusername/codecollab",
-    },
-    {
-      index: 5,
-      name: "RecipeRealm",
-      description: "A community-driven recipe sharing platform where users can discover, share, and rate recipes.",
-      tags: [
-        { name: "Next.js", color: "bg-gray-200" }, // Tailwind class for Next.js dark
-        { name: "GraphQL", color: "bg-pink-200" }, // Tailwind class for GraphQL pink
-        { name: "Community", color: "bg-green-200" }, // Tailwind class for community green
-        { name: "Recipes", color: "bg-orange-200" } // Tailwind class for recipes orange
-      ],
-      source_code_link: "https://github.com/yourusername/reciperealm",
-    },
-    {
-      index: 6,
-      name: "StudyBuddy",
-      description: "A study organization app that helps students manage their study schedules, notes, and collaboration with peers.",
-      tags: [
-        { name: "Svelte", color: "bg-orange-200" }, // Tailwind class for Svelte orange
-        { name: "Ruby on Rails", color: "bg-red-200" }, // Tailwind class for Ruby on Rails red
-        { name: "Education", color: "bg-blue-200" }, // Tailwind class for education blue
-      ],
-      source_code_link: "https://github.com/yourusername/studybuddy",
     },
   ];
 
